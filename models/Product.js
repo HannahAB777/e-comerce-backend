0163fdfef75cfc452799161ba3bc403ddd6f3f2e
hannah AB <hannah.allisonbatt@gmail.com>
2022-03-16 16:11:55 +0800

    connection working products edited

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes, DECIMAL, INTEGER } = require('sequelize');
-const { INSERT } = require('sequelize/types/lib/query-types');
+const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
